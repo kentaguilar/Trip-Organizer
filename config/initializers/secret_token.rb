362830b1af210b343b9dfa97d847274a41a36e95
Kent Aguilar <kentaguilar@gmail.com>
2013-11-18 14:51:25 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pogipoints2::Application.config.secret_token = '9f1e703f17659091bab76beff33b669fce232f3415d60d32cdf01698fc189c9a5a7e9bf0a1066727949caa85cce3dba43387d968ac5aef6d50c5ec2c0df20323'
